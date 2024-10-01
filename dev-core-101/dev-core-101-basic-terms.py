0106f762d64639c792fc07415dae412cf41a17ad
Adilkhan <116016401+adilkhan-tubulbayev@users.noreply.github.com>
2024-10-01 19:41:52 +0500

    Add files via upload

---
+++ dev-core-101/dev-core-101-basic-terms.py
@@ -0,0 +1,37 @@
+#1
+grade = 12
+
+#2
+def sum(num1,num2):
+	return num1 + num2
+
+num1, num2 = map(int, input().split())
+print(sum(num1, num2))
+
+#3
+if grade < 12:
+	print("Relax")
+else:
+	print("Exams are coming...")
+
+#4
+for i in range(1, 10, 2):
+	print(f"Odd number: {i}")
+
+#5
+a = 5
+
+#6
+result = 5 + 8
+
+#7
+num = 111
+
+#8
+pl = "Python"
+
+#9
+#some comments ...
+
+#10
+is_worker = False
