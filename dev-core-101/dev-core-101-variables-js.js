dacc4934ac62899fdf2e41b25415a58c7e13afcb
Adilkhan <116016401+adilkhan-tubulbayev@users.noreply.github.com>
2024-10-02 20:04:35 +0500

    Add files via upload

---
+++ dev-core-101/dev-core-101-variables-js.js
@@ -0,0 +1,8 @@
+console.log(`Hello!
+Please, input 2 numbers`);
+
+let num1 = 5;
+let num2 = 10;
+
+console.log(`Addition of ${num1} and ${num2} equals ${num1 + num2}`);
+console.log(`Substraction of ${num1} and ${num2} equals ${num1 + num2}`);
---
+++ dev-core-101/dev-core-101-variables-python.py
@@ -0,0 +1,6 @@
+print("Hello!")
+num1 = 5
+num2 = 10
+
+print(f"Addition of {num1} and {num2} equals {num1 + num2}")
+print(f"Substraction of {num1} and {num2} equals {num1 - num2}")
