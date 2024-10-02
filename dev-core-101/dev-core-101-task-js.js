d911948663aa6a213522231675705df8679bd5da
Adilkhan <116016401+adilkhan-tubulbayev@users.noreply.github.com>
2024-10-02 19:47:52 +0500

    Add files via upload

---
+++ dev-core-101/dev-core-101-task-js.js
@@ -0,0 +1,10 @@
+//cleaning room
+
+console.log(`1. Collect the trash in the bin".
+2. Put the scattered items back in their places.
+3. Wipe the dust off the surfaces.
+4. Vacuum or sweep the floor.
+5. Wipe the floor with a damp cloth (if necessary).
+6. Air the room.
+7. Straighten the bed
+8. Carefully fold the blankets and pillows.`)
---
+++ dev-core-101/dev-core-101-task-python.py
@@ -0,0 +1,10 @@
+#cooking eggs
+
+print("1. Turn on the stove\n"
+			"2. Put the pan on medium heat.\n"
+			"3. Add a little oil and heat it up.\n" 
+			"4. Break the eggs into a frying pan.\n"
+			"5. Add salt and pepper to taste.\n" 
+			"6. Fry the eggs for 3-5 minutes until the desired consistency.\n" 
+			"7. Remove the pan from the stove\n"
+			"8. Transfer the fried eggs to a plate.\n")
