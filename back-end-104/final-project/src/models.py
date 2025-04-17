aee3b88137afae4f135c6158d9ed9e7f3cbb5cc2
root <root@adilkhan>
2025-04-18 02:02:18 +0500

    Add fullname column migration and update models.py

--- back-end-104/final-project/src/models.py
+++ back-end-104/final-project/src/models.py
@@ -33,6 +33,7 @@ class User(Base):
 
   id: Mapped[int] = mapped_column(primary_key = True, index = True)
   name: Mapped[str] = mapped_column(String(100), unique = True, nullable = False)
+  fullname: Mapped[Optional[str]]
   email: Mapped[str] = mapped_column(String(100), unique = True, nullable = False)
   password_hash: Mapped[str] = mapped_column(String(255), nullable = False)
   role: Mapped[UserRole] = mapped_column(Enum(UserRole), server_default = UserRole.STUDENT.value)
